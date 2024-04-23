0cebea9f4dd73aa3e33aa9276808c49a4d40a3d8
TimMartin147 <65561957+TimMartinRiG147@users.noreply.github.com>
2024-04-23 21:41:05 +0200

    test

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,5 +1,5 @@
-siege -b -t30S -v http://foo.localhost 
-siege -b -t30S -v http://bar.localhost 
+# siege -b -t30S -v http://foo.localhost 
+# siege -b -t30S -v http://bar.localhost 
 
-# siege -b -t30S -v http://foo.localhost > foo-load-test-results.txt
-# siege -b -t30S -v http://bar.localhost > bar-load-test-results.txt
+siege -b -t30S -v http://foo.localhost > foo-load-test-results.txt
+siege -b -t30S -v http://bar.localhost > bar-load-test-results.txt
