a549934e9e4baafa7ad0dfefa4b60c654d0aa1c9
TimMartin147 <65561957+TimMartinRiG147@users.noreply.github.com>
2024-04-23 21:48:37 +0200

    Update load-test.sh

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,5 +1,2 @@
-# siege -b -t30S -v http://foo.localhost 
-# siege -b -t30S -v http://bar.localhost 
-
-siege -b -t30S -v http://foo.localhost > foo-load-test-results.txt
-siege -b -t30S -v http://bar.localhost > bar-load-test-results.txt
+siege -b -t30S -v http://foo.localhost > load-test-results.txt
+siege -b -t30S -v http://bar.localhost >> load-test-results.txt
