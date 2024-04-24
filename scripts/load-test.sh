0c4883d8062b14cbad2016d29d4cae26ab0335e7
TimMartin147 <65561957+TimMartinRiG147@users.noreply.github.com>
2024-04-24 11:16:18 +0200

    test

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,2 +1,5 @@
-siege -b -t30S -v http://foo.localhost > load-test-results.txt
+# siege -b -t30S -v http://foo.localhost > load-test-results.txt
 # siege -b -t30S -v http://bar.localhost >> load-test-results.txt
+
+siege -v -r 10 -c 20 http://foo.localhost > load-test-results.txt
+# siege -v -r 10 -c 20 http://bar.localhost > load-test-results.txt
