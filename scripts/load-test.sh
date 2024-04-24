e057879d5c7d87d344145ce1d4e390241291d25f
TimMartin147 <65561957+TimMartinRiG147@users.noreply.github.com>
2024-04-24 11:46:39 +0200

    clean up

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,12 +1,7 @@
-# siege -b -t30S -v http://foo.localhost > load-test-results.txt
-# siege -b -t30S -v http://bar.localhost >> load-test-results.txt
-
-# siege -v -r 10 -c 20 http://foo.localhost > load-test-results.txt
-# siege -v -r 10 -c 20 http://bar.localhost > load-test-results.txt
 
 # Run siege for "foo"
 echo "Results for foo.localhost:" > siege_results.txt
-siege -v -r 10 -c 20 http://foo.localhost >> siege_results.txt
+siege -v -r 500 -c 50 http://foo.localhost >> siege_results.txt
 # Run siege for "bar"
-echo "\nResults for bar.localhost:" >> siege_results.txt
-siege -v -r 10 -c 20 http://bar.localhost >> siege_results.txt
+echo "Results for bar.localhost:" >> siege_results.txt
+siege -v -r 500 -c 50 http://bar.localhost >> siege_results.txt
