be936bf25651fde116769311cfd598fd6bb646a1
TimMartin147 <65561957+TimMartinRiG147@users.noreply.github.com>
2024-04-24 11:26:19 +0200

    test

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,5 +1,12 @@
 # siege -b -t30S -v http://foo.localhost > load-test-results.txt
 # siege -b -t30S -v http://bar.localhost >> load-test-results.txt
 
-siege -v -r 10 -c 20 http://foo.localhost > load-test-results.txt
+# siege -v -r 10 -c 20 http://foo.localhost > load-test-results.txt
 # siege -v -r 10 -c 20 http://bar.localhost > load-test-results.txt
+
+# Run siege for "foo"
+echo "Results for foo.localhost:" > siege_results.txt
+siege -v -r 10 -c 20 http://foo.localhost >> siege_results.txt
+# Run siege for "bar"
+echo "\nResults for bar.localhost:" >> siege_results.txt
+siege -v -r 10 -c 20 http://bar.localhost >> siege_results.txt
